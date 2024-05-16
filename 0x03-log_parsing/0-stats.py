65ccb6a08fedd604eb08f39f6e6a66872ace5277
elpastore <abdoulayesadio1997@gmail.com>
2024-05-16 16:19:21 +0000

    :( not good

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,65 +6,76 @@ import re
 
 
 def get_input(input_line):
-    """get the iput in the right format."""
-    pattern_parts = (
+    """
+    get the input in the right format
+    """
+    fp = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
         r'\s*(?P<status_code>\S+)',
         r'\s*(?P<file_size>\d+)'
     )
-    pattern = '{}\\-{}{}{}{}\\s*'.format(*pattern_parts)
-    match = re.fullmatch(pattern, input_line)
-    if match:
-        status_code = match.group('status_code')
-        file_size = int(match.group('file_size'))
-        return {'status_code': status_code, 'file_size': file_size}
-    return {'status_code': 0, 'file_size': 0}
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
 def print_statistics(total_file_size, status_codes_stats):
     """
-    Print the statistical info
+    print stat info
     """
-    print('Total file size:', total_file_size)
+    print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
         if num > 0:
-            print(f'{status_code}: {num}')
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
     """
-    Updates information
+    update stat
     """
     line_info = get_input(line)
     status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats:
+    if status_code in status_codes_stats.keys():
         status_codes_stats[status_code] += 1
     return total_file_size + line_info['file_size']
 
 
-def main():
+def run():
     """
     executor
     """
     line_num = 0
     total_file_size = 0
-    status_codes_stats = {'200': 0,
-                          '301': 0,
-                          '400': 0,
-                          '401': 0,
-                          '403': 0,
-                          '404': 0,
-                          '405': 0,
-                          '500': 0
-                          }
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
         while True:
             line = input()
             total_file_size = update_metrics(
-                line, total_file_size, status_codes_stats)
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
             line_num += 1
             if line_num % 10 == 0:
                 print_statistics(total_file_size, status_codes_stats)
@@ -73,4 +84,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    run()
