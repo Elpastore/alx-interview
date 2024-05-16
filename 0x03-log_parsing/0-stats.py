7547ac039c7a427243979446ccb83b22c9e5055a
elpastore <abdoulayesadio1997@gmail.com>
2024-05-16 16:16:26 +0000

    :(

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,58 +3,74 @@
 0-stats module
 """
 import re
-import sys
 
 
-def get_input(line):
-    """
-    get the input
-    """
-    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-    match = re.match(pattern, line)
-    
-    information = {}
-    
+def get_input(input_line):
+    """get the iput in the right format."""
+    pattern_parts = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    pattern = '{}\\-{}{}{}{}\\s*'.format(*pattern_parts)
+    match = re.fullmatch(pattern, input_line)
     if match:
-        ip_address = match.group(1)
-        date = match.group(2)
-        status_code = int(match.group(3))
-        file_size = int(match.group(4))
-        
-        information['File Size'] = file_size
-        information['Status Code'] = status_code
-        
-        return information
-    else:
-        print("Input string does not match the expected format.")
+        status_code = match.group('status_code')
+        file_size = int(match.group('file_size'))
+        return {'status_code': status_code, 'file_size': file_size}
+    return {'status_code': 0, 'file_size': 0}
 
-if __name__ == "__main__":
-    total_size = 0
-    status_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
-                     '403': 0, '404': 0, '405': 0, '500': 0}
-    line_counter = 0
 
+def print_statistics(total_file_size, status_codes_stats):
+    """
+    Print the statistical info
+    """
+    print('Total file size:', total_file_size)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print(f'{status_code}: {num}')
+
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    """
+    Updates information
+    """
+    line_info = get_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats:
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def main():
+    """
+    executor
+    """
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {'200': 0,
+                          '301': 0,
+                          '400': 0,
+                          '401': 0,
+                          '403': 0,
+                          '404': 0,
+                          '405': 0,
+                          '500': 0
+                          }
     try:
-        for line in sys.stdin:
-            information = get_input(line)
-            
-            if information:
-                status_code = str(information.get('Status Code'))
-                total_size += information.get('File Size')
-                status_counts[status_code] += 1
-                line_counter += 1
-                
-                if line_counter % 10 == 0:
-                    print(f"Total file size: {total_size}")
-                    for code, count in sorted(status_counts.items()):
-                        if count > 0:
-                            print(f"{code}: {count}")
-                    #print()
-                    line_counter = 0
-
-    #except KeyboardInterrupt:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line, total_file_size, status_codes_stats)
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
-        print(f"Total file size: {total_size}")
-        for code, count in sorted(status_counts.items()):
-            if count > 0:
-                print(f"{code}: {count}")
+        print_statistics(total_file_size, status_codes_stats)
+
+
+if __name__ == '__main__':
+    main()
