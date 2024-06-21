7c54f8fe39657609f10bc36505d733ca97ef4b14
elpastore <abdoulayesadio1997@gmail.com>
2024-06-21 21:29:37 +0000

    task0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,7 +2,6 @@
 """
 making_change module
 """
-import sys
 
 
 def makeChange(coins, total):
@@ -11,16 +10,13 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [sys.maxsize for i in range(total + 1)]
-    dp[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = dp[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < dp[i]:
-                    dp[i] = subres + 1
 
-    if dp[total] == sys.maxsize:
-        return -1
-    return dp[total]
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
