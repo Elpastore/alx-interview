540fd2d5d5f22290034afb2bdb2642f0d0137f88
elpastore <abdoulayesadio1997@gmail.com>
2024-06-26 13:33:48 +0000

    task0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-island perimeter module
+Island perimeter module
 """
-
 
 def island_perimeter(grid):
     """
-    function that give the perimeter of island
+    Function that calculates the perimeter of an island
     """
 
     rows, cols = len(grid), len(grid[0])
@@ -16,9 +15,9 @@ def island_perimeter(grid):
         for col in range(cols):
             if grid[row][col] == 1:
                 perimeter += 4
-                if row < cols - 1 and grid[row + 1][col] == 1:
+                if row < rows - 1 and grid[row + 1][col] == 1:
                     perimeter -= 2
-                if col < rows - 1 and grid[row][col + 1] == 1:
+                if col < cols - 1 and grid[row][col + 1] == 1:
                     perimeter -= 2
 
     return perimeter
